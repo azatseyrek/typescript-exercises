4fa0bffea38a6f73ec93b4b234f4f7f8f027e51a
Azat Seyrek <azatseyrek@Azat-Mac-mini.local>
2022-06-05 16:19:23 +0300

    useState type

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,57 @@
 import React from 'react'
 import {Button} from './components/Button'
+import Container from './components/Container'
 import Greet from './components/Greet'
 import Input from './components/Input'
 
 import Person from './components/Person'
 import PersonList from './components/PersonList'
+import LoggedIn from './components/state/LoggedIn'
 import Status from './components/Status'
 
 function App() {
-  const personName = {
-    first: 'Bruce',
-    last: 'Wayne',
-  }
+    const personName = {
+        first: 'Bruce',
+        last: 'Wayne',
+    }
 
-  const nameList = [
-    {
-      first: 'Bruce',
-      last: 'Wayne',
-    },
-    {
-      first: 'Clark',
-      last: 'Kent',
-    },
-    {
-      first: 'Lady',
-      last: 'Diana',
-    },
-  ]
+    const nameList = [
+        {
+            first: 'james',
+            last: 'Hardy',
+        },
+        {
+            first: 'Clark',
+            last: 'Kent',
+        },
+        {
+            first: 'Lady',
+            last: 'Diana',
+        },
+    ]
 
-  const [text, setText] = React.useState("")
+    const [text, setText] = React.useState('')
 
-  return (
-    <div className="App">
-      <Greet name="azat" isLoggedIn={true} messageCount={12} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
-      <Status status="error" />
-      <Input
-        value= {text}
-        handleChange={(event) => setText(event.target.value)}
-      />
-      <Button
-        handleClick={(event, id) => {
-          console.log(text , event, id)
-        }}
-      />
-    </div>
-  )
+    return (
+        <div className="App">
+            <Greet name="azat" isLoggedIn={true} messageCount={12} />
+            <Person name={personName} />
+            <PersonList names={nameList} />
+            <Status status="error" />
+            <Input
+                value={text}
+                handleChange={(event) => setText(event.target.value)}
+            />
+            <Button
+                handleClick={(event, id) => {
+                    console.log(text, event, id)
+                }}
+            />
+            <Container styles={{border: '1px solid black', padding: '1rem'}} />
+
+            <LoggedIn />
+        </div>
+    )
 }
 
 export default App
